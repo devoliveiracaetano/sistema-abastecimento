4ed54eccb142468d4553d3c634bdc3e7737dd6d5
Altamir Caetano de Oliveira <altcaeol@gmail.com>
2025-09-17 21:12:41 -0300

    Melhorando layout tela login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import "./Login.css";
+import caminhaoImg from "../assets/caminhao1.jpg";
+import postoImg from "../assets/caminhao4.png";
 
 export default function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-container">
+      <div className="login-background">
+        <img src={postoImg} alt="Posto de gasolina" className="posto-bg" />
+        <img src={caminhaoImg} alt="Caminhão-tanque" className="caminhao-bg" />
+      </div>
+      <div className="login-box">
+        <h1 className="login-title">Logística de Carga e Descarga</h1>
+        <form>
+          <input type="text" placeholder="Usuário" />
+          <input type="password" placeholder="Senha" />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
